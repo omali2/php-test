93ec7ec3eea4f176b1e4f60a2cdd183f1096a290
Saeid Omalzadeh <info@omalzadeh.de>
2017-07-05 10:43:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
-
-echo "Welcome to Openshift das ist nur ein Test mit PHP"
-echo "Hallo Saeid vom ".gethostname()
-
+// Create a 55x30 image
+$im = imagecreatetruecolor(200, 200);
+$white = imagecolorallocate($im, 255, 255, 255);
+$blue = imagecolorallocate($im, 0, 0, 255);
+$green = imagecolorallocate($im, 0, 255, 0);
+// Draw a filled rectangle
+imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+//imagefilledrectangle($im, 0, 0, 199, 199, $green);
+// Save the image
+header('Content-Type: image/png');
+imagePNG($im);
+imagedestroy($im);
 ?>
