731f0db467e328f72c7747c8a58b73d85d6f1049
Saeid Omalzadeh <info@omalzadeh.de>
2017-08-23 11:52:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,17 @@
 <?php
-
-// Create a 55x30 image
-//$im = imagecreatetruecolor(200, 200);
-//$white = imagecolorallocate($im, 255, 255, 255);
-//$blue = imagecolorallocate($im, 0, 0, 255);
-//$green = imagecolorallocate($im, 0, 255, 0);
-/// Draw a filled rectangle
-//imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-///imagefilledrectangle($im, 0, 0, 199, 199, $green);
-/// Save the image
-//header('Content-Type: image/png');
-//imagePNG($im);
-//imagedestroy($im);
-
-?>
-
-<?php
-
-echo "Hallo Saeid vom Server = ".gethostname()
-
-?>
-
-<?php
 $dbhost = "172.30.89.192";
 $dbport = "3306";
 $dbuser = "root";
 $dbpwd = "omali";
 $dbname = "saeid";
 
-//$dbhost = getenv("MYSQL_SERVICE_HOST");
-//$dbport = getenv("MYSQL_SERVICE_PORT");
-//$dbuser = getenv("MYSQL_USER");
-//$dbpwd = getenv("MYSQL_PASSWORD");
-//$dbname = getenv("MYSQL_DATABASE");
-
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-echo "<B>Name     Alte</B><BR>";
-echo "----------------------------------------------<BR>";
+echo "Name  Alte <BR>";
+echo "----------------------------------------<br>";
 $query = "SELECT * from costumer";
 $rs = $connection->query($query);
 while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
@@ -48,5 +19,4 @@ while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
 }
 $rs->close();
 $connection->close();
-
 ?>
