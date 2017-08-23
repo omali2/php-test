f3dfd52016d276740dd3a770412433ad44174bfd
Saeid Omalzadeh <info@omalzadeh.de>
2017-08-23 13:21:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
+
 $dbhost = "172.30.89.192";
 $dbport = "3306";
 $dbuser = "root";
 $dbpwd = "omali";
 $dbname = "saeid";
+$dbtable= "customer"
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-echo "Name  Alte <BR>";
-echo "----------------------------------------<br>";
-$query = "SELECT * from costumer";
+echo "<B> Name  Alte <br>";
+echo "------------------------------------<BR>";
+$query = "SELECT * from customer";
 $rs = $connection->query($query);
 while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
-    echo $row['name'] . " " . $row['age'] . "\n <BR>";
+    echo $row['name'] . " " . $row['age'] . "\n";
 }
 $rs->close();
 $connection->close();
+
 ?>
